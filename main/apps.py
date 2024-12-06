dd4a2c4763c84520dda8204c050eae1dd16553ea
steve-ongera <gadafisteve001@gmail.com>
2024-12-07 00:57:00 +0300

    modified apps.py for signals

--- main/apps.py
+++ main/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
-
-class MainConfig(AppConfig):
+class MainConfig(AppConfig):  # Replace "Main" with your app name
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'main'
+
+    def ready(self):
+        import main.signals  # Ensure your signals are imported
+
